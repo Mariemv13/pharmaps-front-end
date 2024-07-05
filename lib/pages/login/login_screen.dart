7fc9866fabf97873ea68b6cdcfdecdae94b7063e
Mariemev13 <touremami2002@gmail.com>
2024-07-05 16:26:02 +0000

    add login page

--- lib/pages/login/login_screen.dart
+++ lib/pages/login/login_screen.dart
@@ -1,10 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:pharmaps/pages/register/register_screen.dart';
+import 'package:pharmaps/utils/constants.dart';
+import 'package:pharmaps/widgets/button_primary.dart';
+import 'package:pharmaps/widgets/logo_space.dart';
 
-class LoginScreen extends StatelessWidget {
+
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool _secureText = true;
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          const LogoSpace(),
+          Container(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 24,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Color(0x40000000),
+                            offset: Offset(0, 1),
+                            blurRadius: 4,
+                            spreadRadius: 0)
+                      ],
+                      color: whiteColor),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Adresse mail',
+                        hintStyle: lightTextStyle.copyWith(
+                            fontSize: 15, color: greyLightColor)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 16),
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Color(0x40000000),
+                            offset: Offset(0, 1),
+                            blurRadius: 4,
+                            spreadRadius: 0)
+                      ],
+                      color: whiteColor),
+                  width: MediaQuery.of(context).size.width,
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: _secureText,
+                    decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: showHide,
+                          icon: _secureText
+                              ? const Icon(
+                                  Icons.visibility_off,
+                                  size: 20,
+                                )
+                              : const Icon(
+                                  Icons.visibility,
+                                  size: 20,
+                                ),
+                        ),
+                        border: InputBorder.none,
+                        hintText: 'Mot de passe',
+                        hintStyle: lightTextStyle.copyWith(
+                            fontSize: 15, color: greyLightColor)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ButtonPrimary(
+                    text: "Se Connecter",
+                    onPressed: () {
+                      if (emailController.text.isEmpty ||
+                          passwordController.text.isEmpty) {
+                        showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                                  title: const Text("Attention !!"),
+                                  content:
+                                      const Text("Veuillez entrer tous les champs"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text("Ok"))
+                                  ],
+                                ));
+                      } else {
+                        //submitLogin();
+                      }
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Pas de compte? ",
+                      style: lightTextStyle.copyWith(
+                          color: greyLightColor, fontSize: 15),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterScreen()),
+                            (route) => false);
+                      },
+                      child: Text(
+                        "Créer un compte",
+                        style: boldTextStyle.copyWith(
+                            color: greenColor, fontSize: 15),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
