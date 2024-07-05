91b87dbce491ec6c39b2acb503094816acd01c4f
Josué DA-MATHA <josuedamatha2001@gmail.com>
2024-07-05 23:58:31 +0200

    add login and register url

---
+++ lib/api/url_api.dart
@@ -0,0 +1,5 @@
+class BASEURL {
+  static String ipAddress = "127.0.0.1";
+  static String apiRegister = "http://$ipAddress/phmps/register";
+  static String apiLogin = "http://$ipAddress/phmps/login";
+}
